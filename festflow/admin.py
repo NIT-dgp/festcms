47513f7d8783a69edaff33d096ac60d20b2245bb
Naveen Kumar Sangi <nkprince007@gmail.com>
2016-11-19 07:36:57 +0530

    Show event details in admin profile
    Fixes #44

--- festflow/admin.py
+++ festflow/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from .models import *
 # Register your models here.
+class EventAdmin(admin.ModelAdmin):
+    list_display=('name', 'description', 'venue', 'date_time')
+
 admin.site.register(Profile)
-admin.site.register(Event)
+admin.site.register(Event, EventAdmin)
 admin.site.register(organizerMember)
 admin.site.register(sponsor)
